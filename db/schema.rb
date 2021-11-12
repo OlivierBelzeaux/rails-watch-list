4391dfaf59671e6cb52d9f07a1ec9d2f844b85f8
Olivier Belzeaux <olivierbelzeaux@gmail.com>
2021-11-12 15:24:32 +0100

    add card + img cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_105918) do
+ActiveRecord::Schema.define(version: 2021_11_12_133931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,17 @@ ActiveRecord::Schema.define(version: 2021_11_12_105918) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
+    t.bigint "lists_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lists_id"], name: "index_reviews_on_lists_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
+  add_foreign_key "reviews", "lists", column: "lists_id"
 end
